aa56e85de60d93892c1130eecee118a6be06c994
Alex DiLiberto <alex@alexdiliberto.com>
2018-01-17 23:09:58 -0500

    test(acceptance): update to use modern testing syntax

--- tests/acceptance/milligram-import-styles-test.js
+++ tests/acceptance/milligram-import-styles-test.js
@@ -1,18 +1,20 @@
-import { test } from 'qunit';
-import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import { visit } from '@ember/test-helpers';
 
-moduleForAcceptance('Acceptance | milligram import styles');
+module('Acceptance | milligram import styles', function(hooks) {
+  setupApplicationTest(hooks);
 
-test('milligram styles are imported', async function(assert) {
-  await visit('/');
+  test('milligram styles are imported', async function(assert) {
+    await visit('/');
 
-  // https://github.com/milligram/milligram/blob/v1.3.0/dist/milligram.css#L45
-  let btn = this.element.querySelector('#buttons a.button');
-  let btnBackgroundColor = window
-    .getComputedStyle(btn)
-    .getPropertyValue('background-color')
+    // https://github.com/milligram/milligram/blob/v1.3.0/dist/milligram.css#L45
+    let btn = this.element.querySelector('#buttons a.button');
+    let btnBackgroundColor = window
+      .getComputedStyle(btn)
+      .getPropertyValue('background-color')
 
-  // http://rgb.to/hex/9b4dca
-  assert.equal(btnBackgroundColor, 'rgb(155, 77, 202)');
+    // http://rgb.to/hex/9b4dca
+    assert.equal(btnBackgroundColor, 'rgb(155, 77, 202)');
+  });
 });
